1cb0010052b08dca8c09b49a83143cc3be20b968
Evan McEldowney <evanmac123@gmail.com>
2017-01-24 16:51:54 -0500

    fixed host

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ var connectionString = 'mongodb://127.0.0.1:27017/test';
 if(process.env.MLAB_USERNAME) {
     connectionString = process.env.MLAB_USERNAME + ":" +
         process.env.MLAB_PASSWORD + "@" +
-        process.env.P + ':' +
+        process.env.MLAB_HOST + ':' +
         process.env.MLAB_PORT + '/' +
         process.env.MLAB_APP_NAME;
 }
-
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
