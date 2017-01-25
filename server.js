0ea4e9794d2e7dbff4876f2a843e709511d28372
Evan McEldowney <evanmac123@gmail.com>
2017-01-25 13:26:10 -0500

    messed up on enviorment

--- server.js
+++ server.js
@@ -1,39 +1,40 @@
 var express = require('express');
-var app = express();
+//var mongoose = require('mongoose');
 
+// var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test';
+//
+// // Connect to the DB
+// mongoose.connect(url);
+//
+// var db = mongoose.connection;
+//
+// db.on("error", console.error.bind(console, "connection error"));
+// db.once("open", function(callback) {
+//     console.log("Connection succeeded, connected to db instance. Url is " + url);
+// });
 var connectionString = 'mongodb://127.0.0.1:27017/test';
-if(process.env.MLAB_USERNAME) {
-    connectionString = process.env.MLAB_USERNAME + ":" +
-        process.env.MLAB_PASSWORD + "@" +
-        process.env.MLAB_HOST + ':' +
-        process.env.MLAB_PORT + '/' +
-        process.env.MLAB_APP_NAME;
+if(process.env.MONGOLAB_URI){
+    connectionString = process.env.MONGOLAB_URI
 }
+// if(process.env.MLAB_USERNAME) {
+//     connectionString = process.env.MLAB_USERNAME + ":" +
+//         process.env.MLAB_PASSWORD + "@" +
+//         process.env.MLAB_HOST + ':' +
+//         process.env.MLAB_PORT + '/' +
+//         process.env.MLAB_APP_NAME;
+// }
+
+var app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var passport      = require('passport');
-var cookieParser  = require('cookie-parser');
-var session       = require('express-session');
-
-app.use(session({
-    secret: 'this is the secret',
-    resave: true,
-    saveUninitialized: true
-}));
-app.use(cookieParser());
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.set('view engine', 'ejs');
-
+// configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
-require("./todo/app")(app);
 
-var port      = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
